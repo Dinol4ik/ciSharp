cf45c7baae5ca3119bbbcb600dddb56aa1ffb384
Irka-Probirka <ankuznecov42@yandex.ru>
2023-06-08 02:41:56 +0500

    handler for organization - done

--- Проект Отлов животных/OrganizationC.cs
+++ Проект Отлов животных/OrganizationC.cs
@@ -6,25 +6,25 @@ using System.Threading.Tasks;
 
 namespace Проект_Отлов_животных
 {
-    public class Organization
+    public class OrganizationC
     {
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Type { get; set; }
         public string? Inn { get; set; }
         public string? Kpp { get; set; }
-        public string? Adress { get; set; }
+        public string? RegisterAdress { get; set; }
         public string? IpOrOoo { get; set; }
         public string? Locality { get; set; }
 
-        public Organization(int id, string title, string? type, string inn, string? kpp, string? adress, string? iporooo, string? locality)
+        public OrganizationC(int id, string title, string? type, string inn, string? kpp, string? registerAdress, string? iporooo, string? locality)
         {
             Id = id;
             Title = title;
             Type = type;
             Inn = inn;
             Kpp = kpp;
-            Adress = adress;
+            RegisterAdress = registerAdress;
             IpOrOoo = iporooo;
             Locality = locality;
         }
--- Проект Отлов животных/SearchOrganisation.cs
+++ Проект Отлов животных/SearchOrganisation.cs
@@ -2,24 +2,92 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Contracts;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static Проект_Отлов_животных.Models;
 
 namespace Проект_Отлов_животных
 {
     public partial class SearchOrganisation : Form
     {
-        public SearchOrganisation()
+        public SearchOrganisation(List<OrganizationC> org, List<Models.Locality> locality, List<Models.Type_Of_Organization> type)
         {
+            data = org;
             InitializeComponent();
+
+            comboBox1.DataSource = locality;
+            comboBox1.DisplayMember = "Adress";
+            comboBox1.ValueMember = "Id";
+
+            comboBox2.DataSource = type;
+            comboBox2.DisplayMember = "Title";
+            comboBox2.ValueMember = "Id";
+
+            dataGridView1.DataSource = org.GetRange(0, org.Count);
+            label2.Text = dataGridView1.Rows[0].Cells[0].Value.ToString();
         }
+        List<OrganizationC> data;
 
         private void button1_Click(object sender, EventArgs e)
         {
             string title = textBox1.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Изменить
+        {
+            int id = int.Parse(label2.Text);
+
+            Models.Locality localityId = (Models.Locality)comboBox1.SelectedItem;
+            Models.Type_Of_Organization typeOrgId = (Models.Type_Of_Organization)comboBox2.SelectedItem;
+            Models.Organization organization = new Models.Organization
+            {
+                Id = int.Parse(label2.Text),
+                Title = textBox2.Text,
+                TypeOfId = typeOrgId.Id,
+                INN = textBox3.Text,
+                KPP = textBox4.Text,
+                Sole_Properietor = textBox5.Text,
+                Registation_addres = textBox6.Text,
+                LocalityId = localityId.Id,
+        };
+            //if (data.Find(x => x.Id == id) != null) { data.Remove(data.Find(x => x.Id == id)); }
+            RegisterOrganization organizationHandler = new RegisterOrganization();
+            organizationHandler.EditOrganization(organization);
+
+            dataGridView1.DataSource = data.GetRange(0, data.Count);
+            dataGridView1.Refresh();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            label2.Text = dataGridView1[0, dataGridView1.CurrentRow.Index].Value.ToString();
+            var title = dataGridView1[1, dataGridView1.CurrentRow.Index].Value.ToString();
+            var inn = dataGridView1[3, dataGridView1.CurrentRow.Index].Value.ToString();
+            var kpp = dataGridView1[4, dataGridView1.CurrentRow.Index].Value.ToString();
+            var ipOrOoo = dataGridView1[5, dataGridView1.CurrentRow.Index].Value.ToString();
+
+            textBox2.Text = title;
+            textBox3.Text = inn;
+            textBox4.Text = kpp;
+            textBox5.Text = ipOrOoo;
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Удалить
+        {
+            int id = int.Parse(label2.Text);
+
+            if (data.Find(x => x.Id == id) != null) { data.Remove(data.Find(x => x.Id == id)); }
+            RegisterOrganization organizationHandler = new RegisterOrganization();
+            organizationHandler.DeleteOrganization(id);
+            dataGridView1.DataSource = data.GetRange(0, data.Count);
+            dataGridView1.Refresh();
         }
     }
 }
